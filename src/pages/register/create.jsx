961ed01549c3d7451eaa809af30f1c55ae8419d4
nlprasanna5 <lakshmi.76740@gmail.com>
2023-04-28 03:35:01 +0530

    added responsive behaviour to login and register pages

--- src/pages/register/create.jsx
+++ src/pages/register/create.jsx
@@ -1,14 +1,15 @@
-import {FcGoogle} from 'react-icons/fc'
-import {BsApple} from 'react-icons/bs'
+import { FcGoogle } from 'react-icons/fc'
+import { BsApple } from 'react-icons/bs'
 import styles from './create.module.css'
-import {BsTwitter} from 'react-icons/bs'
+import { BsTwitter } from 'react-icons/bs'
 import { useRecoilState } from 'recoil'
 import { createState } from '../../atoms/atoms'
 import { useNavigate } from 'react-router-dom'
+import Divider from '@mui/material/Divider';
 
 export function Create() {
 
-// console.log(setCreate, create)
+   
     const [create, setCreate] = useRecoilState(createState)
 
     const navigate = useNavigate();
@@ -20,25 +21,32 @@ export function Create() {
     return (
         <div className={styles.main}>
             <div className={styles.btnContainer}>
-                <BsTwitter style={{color:"#05BFDB",fontSize:'1.5rem'}}/>
+                <BsTwitter style={{ color: "#05BFDB", fontSize: '1.5rem' }} />
                 <h2 className={styles.createHead}>Join Twitter today</h2>
 
                 <button className={styles.btn}><FcGoogle />Sign up with Google</button>
 
-                <button className={styles.btn}><BsApple/>Sign up with Apple</button>
-                
-                {/* <span className={styles.hr}></span> */}
-                <span>_________________ <span>or</span>__________________</span>  
-                <div className={styles.termsdiv}>
-                <button onClick={handleCreate} className={styles.btnAccount}>Create account</button>
-                {/* <div className={styles.termsdiv}> */}
-                <p className={styles.terms}>By signing up, you agree to the<span className={styles.conditions}>Terms of Service</span> 
-                and <span className={styles.conditions}>Privacy Policy</span>, including <span className={styles.conditions}>Cookie Use</span></p>
-                <p className={styles.haveAcc}>Have an account already?<span onClick={()=>navigate('/login')} className={styles.login}>Log in</span></p>
+                <button className={styles.btn}><BsApple />Sign up with Apple</button>
+
+               
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                    <span style={{ flexGrow: 1 ,width:'9vw'}}>
+                        <Divider />
+                    </span>
+                    <span style={{ margin: '0 8px' }}>or</span>
+                    <span style={{ flexGrow: 1,width:'9vw'}}>
+                        <Divider />
+                    </span>
                 </div>
-                {/* <div className={styles.haveAccP}>
-                <p className={styles.haveAcc}>Have an account already?<span className={styles.conditions}>Log in</span></p>
-                </div> */}
+
+                <div className={styles.termsdiv}>
+                    <button onClick={handleCreate} className={styles.btnAccount}>Create account</button>
+                    
+                    <p className={styles.terms}>By signing up, you agree to the<span className={styles.conditions}>Terms of Service</span>
+                        and <span className={styles.conditions}>Privacy Policy</span>, including <span className={styles.conditions}>Cookie Use</span></p>
+                    <p className={styles.haveAcc}>Have an account already?<span onClick={() => navigate('/login')} className={styles.login}>Log in</span></p>
+                </div>
+                
             </div>
         </div>
     );
